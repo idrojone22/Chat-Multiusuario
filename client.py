9e955765124b1d7329b5d796060783cec5e3b583
Jordi Valls <jordivallsplasmx@gmail.com>
2024-02-12 23:04:13 +0100

    1.0.3

--- client.py
+++ client.py
@@ -1,27 +1,8 @@
-#!/usr/bin/env python3
+import socket
+import threading
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 
-import socket # Para creat un socket y conectarse al servidor
-import threading # Para crear hilos para manejar el envío y recepción de mensajes
-from tkinter import * # Para crear la interfaz gráfica de usuario
-from tkinter.scrolledtext import ScrolledText # Para crear un widget de texto con desplazamiento para mostrar mensajes
-
-
-"""
-Esta función se encarga de enviar mensajes al servidor. Recibe cuatro argumentos:
-
-client_socket: El objeto de socket del cliente
-username: El nombre de usuario del cliente
-text_widget: El widget de texto donde se mostrarán los mensajes
-entry_widget: El widget de entrada donde se escribe el mensaje que se enviará
-
-La función comienza recuperando el mensaje del widget de entrada. 
-Luego, formatea el mensaje con el nombre del usuario, concatenando ambos con un símbolo de mayor que ">". 
-A continuación, codifica el mensaje en UTF-8 y lo envía al servidor utilizando el objeto de socket del cliente.
-
-Una vez que el mensaje se envía, la función borra el widget de entrada para que el usuario pueda escribir un nuevo mensaje. 
-Luego, inserta el mensaje enviado en el historial de chat, que se muestra en el widget de texto. 
-Por último, deshabilita el widget de texto para que el usuario no pueda modificarlo hasta que haya enviado un nuevo mensaje.
-"""
 def send_message(client_socket, username, text_widget, entry_widget):
     message = entry_widget.get()
     client_socket.sendall(f"{username} > {message}".encode())
@@ -31,21 +12,8 @@ def send_message(client_socket, username, text_widget, entry_widget):
     text_widget.insert(END, f"{username} > {message}\n")
     text_widget.configure(state='disable')
 
-"""
-Esta función se encarga de recibir mensajes del servidor. Recibe dos argumentos:
-
-client_socket: El objeto de socket del cliente
-text_widget: El widget de texto donde se mostrarán los mensajes
-
-La función comienza creando un bucle infinito que se ejecutará continuamente para recibir mensajes del servidor. 
-Dentro del bucle, la función intenta recibir un mensaje del servidor, leyendo hasta 1024 bytes a la vez.
-
-Si el mensaje recibido no está vacío, la función lo formatea con el nombre del usuario, concatenando ambos con un símbolo de mayor que ">". 
-A continuación, inserta el mensaje recibido en el historial de chat, que se muestra en el widget de texto. 
-Por último, deshabilita el widget de texto para que el usuario no pueda modificarlo hasta que haya recibido un nuevo mensaje.
-"""
-def receive_message(client_socket, text_widget):
-    while True:
+def receive_message(client_socket, text_widget, username, window_closed):
+    while not window_closed:
         try:
             message = client_socket.recv(1024).decode()
 
@@ -59,22 +27,15 @@ def receive_message(client_socket, text_widget):
         except:
             break
 
-"""
-Esta función es el programa principal del cliente. 
-Se encarga de crear la conexión con el servidor, configurar la interfaz gráfica de usuario y manejar el envío y recepción de mensajes.
+def list_users_request(client_socket):
+    client_socket.sendall("!usuarios".encode())
 
-La función comienza definiendo la dirección del host (localhost) y el número de puerto (12345) para el servidor. 
-A continuación, crea un objeto de socket, lo conecta a la dirección y puerto del servidor especificados y establece una conexión con el servidor.
+def exit_request(client_socket, username, window, window_closed):
+    window_closed = True
+    client_socket.sendall(f"\n[!] El usuario {username} ha abandonado el chat.\n\n".encode())
+    client_socket.close()
+    window.quit()
 
-Luego, la función solicita al usuario que ingrese su nombre de usuario, lo codifica en UTF-8 y lo envía al servidor.
-
-A continuación, la función crea una nueva ventana de Tkinter y le da el título "Chat". 
-Luego, crea un widget de texto con desplazamiento para mostrar los mensajes. 
-El widget de texto se deshabilita inicialmente para que el usuario no pueda modificarlo hasta que haya enviado un mensaje.
-
-Finalmente, la función crea un botón para enviar mensajes.
- El botón está vinculado a la función send_message(), que se encargará de enviar el mensaje al servidor.
-"""
 def client_program():
     host = '192.168.1.44'
     port = 12345
@@ -87,29 +48,33 @@ def client_program():
 
     window = Tk()
     window.title("Chat")
+    window_closed = False  # Variable de bandera para indicar si la ventana ha sido cerrada
 
-    # ScrolledText widget
-    text_widget = ScrolledText(window, state='disable')
+    text_widget = ScrolledText(window, state='disabled')
     text_widget.pack(padx=5, pady=5)
 
-    # Button send 
     frame_widget = Frame(window)
-    frame_widget.pack(padx=5, pady=5, fill=BOTH, expand=1)
-    
-    button_widget = Button(frame_widget, text="Enviar")
+    frame_widget.pack(padx=5, pady=2, fill=BOTH, expand=1)
+
+    entry_widget = Entry(frame_widget, font=("Arial", 14))
+    entry_widget.bind("<Return>", lambda _: send_message(client_socket, username, text_widget, entry_widget))
+    entry_widget.pack(side=LEFT, fill=X, expand=1)
+
+    button_widget = Button(frame_widget, text='Enviar', command=lambda: send_message(client_socket, username, text_widget, entry_widget))
     button_widget.pack(side=RIGHT, padx=5)
 
-    # Entry widget
-    entry_widget = Entry(frame_widget)
-    entry_widget.bind("<Return>", lambda _: send_message(client_socket, username, text_widget, entry_widget))
-    entry_widget.pack(side=LEFT, fill=BOTH, expand=1)
+    users_widget = Button(window, text="Listar usuarios", command=lambda: list_users_request(client_socket))
+    users_widget.pack(padx=5, pady=5)
 
-    # Start a thread for receiving messages
-    thread = threading.Thread(target=receive_message, args=(client_socket, text_widget))
+    exit_widget = Button(window, text="Salir", command=lambda: exit_request(client_socket, username, window, window_closed))
+    exit_widget.pack(padx=5, pady=5)
+
+    thread = threading.Thread(target=receive_message, args=(client_socket, text_widget, username, window_closed))
     thread.daemon = True
     thread.start()
 
     window.mainloop()
+    window_closed = True  # Marcar que la ventana se ha cerrado
     client_socket.close()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,65 +1,50 @@
-#!/usr/bin/env python3
-
-import socket # Para crear sockets
-import threading # Para crear hilos 
+import socket
+import threading
 
 def client_thread(client_socket, clients, usernames):
+    username = client_socket.recv(1024).decode()
+    
+    if username in usernames.values():
+        print(f"\n[!] El nombre de usuario '{username}' ya está en uso. Cerrando conexión.")
+        client_socket.sendall("[!] Nombre de usuario ya en uso. Por favor, elige otro.\n".encode())
+        client_socket.close()
+        return
+    
+    usernames[client_socket] = username
+    print(f"\n[+] El usuario {username} se ha conectado al chat")
 
-    """
-    Esta función se encarga de manejar una sola conexión de cliente.
-
-    Recibe tres argumentos:
-    client_socket: socket para el cliente conectado
-    clients: lista de todos los sockets con clientes conectados
-    usernames: diccionario que mapea los sockets de cliente a sus nombres de usuario
-    """
-
-    username = client_socket.recv(1024).decode() # Lee hasta 1024 bytes dek siket y los decodifica como UTF-8
-    usernames[client_socket] = username  # Almacena el nombre de usuario asignado al socket del cliente al nombre de usuario
-    print(f"\n[+] El usuario {username} se ha conectado al chat") # Imprime el mensaje cada vez que un usuario se conecta 
-
-    """
-    Este bucle envia un mensaje a todos los clientes conectados excepto al que ha entrado, informando de
-    que un nuevo usuario ha entrado al chat
-    """
     for client in clients:
-
         if client is not client_socket:
             client.sendall(f"\n[+] El usuario {username} ha entrado al chat\n\n".encode())
 
-    while True: # Este bucle se ejecuta continuamente mientras el usuario esta conectado.
-
-        """
-        Intenta recibir un mensaje del cliente. 
-        Con el metodo recv() vuelve a leer hasta 1024 bytes del socket y los decodifica como UTF-8
-        Luego verificamos si el mensaje esta vacio, lo qu eindica que el cliente esta desconectado. 
-        En caso de ser asi sale del bucle
-        """
+    while True:
         try: 
             message = client_socket.recv(1024).decode()
 
             if not message:
                 break
             
-            #Este bucle envia el mensaje recibido a todos los clientes conectados excpeto al cliente actual
+            if message == "!usuarios":
+                client_socket.sendall(f"\n[+] Listado de usuarios disponibles: {','.join(usernames.values())}\n\n".encode())
+                continue
+
             for client in clients:
                 if client is not client_socket:
                     client.sendall(f"{message}\n".encode())
         
-        # Captura cualquier error que pueda ocurrir durante la cominicación con el cliente y sale del bucle
         except:
             break
-    
-def server_program(): # Programa principal del servidor.
 
-    # Crea un socket, lo asocia a un puerto y comienza a escuchar conexiones entarntes
+    client_socket.close()
+    clients.remove(client_socket)
+    del usernames[client_socket]
 
+def server_program(): 
     host = '192.168.1.44'
     port = 12345
 
-    # Crean un objeto socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # TIME_WAIT
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((host, port))
     server_socket.listen()
 
@@ -68,13 +53,7 @@ def server_program(): # Programa principal del servidor.
     clients = []
     usernames = {}
     
-    """
-    Espera una conexión entrante y la acepta. 
-    El método accept() devuelve una tupla que contiene el objeto de socket del cliente y la dirección del cliente. 
-    El socket del cliente se agrega a la lista clients.
-    """
     while True:
-        
         client_socket, address = server_socket.accept()
         clients.append(client_socket)
 
@@ -87,5 +66,4 @@ def server_program(): # Programa principal del servidor.
     server_socket.close()
 
 if __name__ == '__main__':
-    
     server_program()
